39d2216dd752685c4ee8e66d61c26b1af137ad6f
IKDrocket <yusuke1128rocket@gmail.com>
2019-09-03 04:40:34 +0900

    add tqdm

--- LC_result_extracter.py
+++ LC_result_extracter.py
@@ -4,6 +4,8 @@
 import os
 import glob
 import argparse as ap
+from tqdm.auto import tqdm
+import time
 
 def argparser():
     parser = ap.ArgumentParser()
@@ -16,7 +18,7 @@ def argparser():
     return args
 
 
-def result_extracter(line, result,r_time, r_time_flag):
+def result_extracter(line, result,r_time, r_time_flag, file):
     line_list = line.split("\t")
     r_time_count = 0
     for r in r_time:
@@ -31,9 +33,6 @@ def result_extracter(line, result,r_time, r_time_flag):
         elif line_list == ['\n']:
             break
         r_time_count += 1
-        
-
-
 
 def main():
     args = argparser()
@@ -52,26 +51,34 @@ def main():
     header = ""
     result = []
     file_list = glob.glob(input_folder + "/*.txt")
-    for file in file_list:
-        read_flag = False
-        r_time_flag = [str(0)]*len(r_time)
-        with open(file, 'r',encoding='shift_jis')as f:
-            result.append(file+",")
-            line = f.readline()
-            while line:
-                while not read_flag:
-                    if line.startswith("Peak#"):
-                        header = line
-                        read_flag = True
-                        line = f.readline()
-                        break
-                    else:
-                        line = f.readline()
-                result2 = result_extracter(line,result, r_time, r_time_flag)
+    print("\n"+'===Start extraction==='+"\n")
+    with tqdm(file_list,
+            total=len(file_list),
+            desc="Progress rate",
+            dynamic_ncols=True,
+            leave=False) as pbar:
+        for file in file_list:
+            read_flag = False
+            r_time_flag = [str(0)]*len(r_time)
+            with open(file, 'r',encoding='shift_jis')as f:
+                result.append(file+",")
                 line = f.readline()
-            result_list = ",".join(r_time_flag)
-            result.append(result_list+"\n")
-        
+                while line:
+                    while not read_flag:
+                        if line.startswith("Peak#"):
+                            header = line
+                            read_flag = True
+                            line = f.readline()
+                            break
+                        else:
+                            line = f.readline()
+                    result_extracter(line,result, r_time, r_time_flag, file)
+                    line = f.readline()
+                result_list = ",".join(r_time_flag)
+                result.append(result_list+"\n")
+            pbar.update(1)
+    print(pbar) 
+    print("Program done.")
     header_list = header.split(",")
     result2 = " ".join(result)
     with open(input_folder+"/output.csv", 'w')as f:
